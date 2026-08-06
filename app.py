6ef7d284349f9d7b0c61a85c6ad262f9f9491d80
Brayan <brayan.garcia.dev@gmail.com>
2026-08-06 10:40:51 -0500

    Corrigiendo espacios e indentacion en app.py

--- app.py
+++ app.py
@@ -15,20 +15,20 @@ def inicio():
         conn = conectar_db()
         cur = conn.cursor()
 
-	cur.execute("CREATE TABLE IF NOT EXISTS visitas (id SERIAL PRIMARY KEY, fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP);")
+        cur.execute("CREATE TABLE IF NOT EXISTS visitas (id SERIAL PRIMARY KEY, fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP);")
 
-	cur.execute("INSERT INTO visitas DEFAULT VALUES;")
-	conn.commit()
+        cur.execute("INSERT INTO visitas DEFAULT VALUES;")
+        conn.commit()
 
-	cur.execute("SELECT COUNT(*) FROM visitas;")
-	total_visitas = cur.fetchone()[0]
+        cur.execute("SELECT COUNT(*) FROM visitas;")
+        total_visitas = cur.fetchone()[0]
 
-	cur.close()
-	conn.close()
+        cur.close()
+        conn.close()
 
-	return f"<h1>¡Hola DevOps!</h1><p>Esta página se conecta a PostgreSQL.</p><p>Total de visitas registradas en la base de datos: <b>{total_visitas}</b></p>"
+        return f"<h1>¡Hola DevOps!</h1><p>Esta página se conecta a PostgreSQL.</p><p>Total de visitas registradas en la base de datos: <b>{total_visitas[0]}</b></p>"
     except Exception as e:
-	return f"<h1>Error de conexión</h1><p>{str(e)}</p>"
+        return f"<h1>Error de conexión</h1><p>{str(e)}</p>"
 
 if __name__== '__main__':
     app.run(host='0.0.0.0', port=5000)
