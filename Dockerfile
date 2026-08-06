7c183386db64f5ff77a3c8f397ed0d5bbd856983
Brayan <brayan.garcia.dev@gmail.com>
2026-08-06 10:14:40 -0500

    Agregando backend en Python y conexion a base de datos

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,12 @@
-FROM nginx:alpine
-COPY index.html /usr/share/nginx/html
-EXPOSE 80
+FROM python:3.11-slim
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY app.py .
+
+EXPOSE 5000
+
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:app"]
---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+def conectar_db():
+    return psycopg2.connect(DATABASE_URL)
+
+@app.route('/')
+def inicio():
+    try:
+        conn = conectar_db()
+        cur = conn.cursor()
+
+	cur.execute("CREATE TABLE IF NOT EXISTS visitas (id SERIAL PRIMARY KEY, fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP);")
+
+	cur.execute("INSERT INTO visitas DEFAULT VALUES;")
+	conn.commit()
+
+	cur.execute("SELECT COUNT(*) FROM visitas;")
+	total_visitas = cur.fetchone()[0]
+
+	cur.close()
+	conn.close()
+
+	return f"<h1>¡Hola DevOps!</h1><p>Esta página se conecta a PostgreSQL.</p><p>Total de visitas registradas en la base de datos: <b>{total_visitas}</b></p>"
+    except Exception as e:
+	return f"<h1>Error de conexión</h1><p>{str(e)}</p>"
+
+if __name__== '__main__':
+    app.run(host='0.0.0.0', port=5000)
